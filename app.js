3a557552f4f9099d33ae5ba63391ead152d9e01a
Madhan Raju J <Madhan.J@BRILLIO.COM>
2017-08-11 20:48:19 +0530

    updated

--- app.js
+++ app.js
@@ -102,7 +102,7 @@ var intents1 = new builder.IntentDialog({ recognizers: [recognizer_api] }); //ap
 });*/
 
 //bot.dialog('/',intents); 
-bot1.dialog('/',intents1); 
+//bot1.dialog('/',intents1); 
 
 intents1.matches('whatIsWeather',[ function(session,args)
 { var city11 = builder.EntityRecognizer.findEntity(args.entities,'city'); 
